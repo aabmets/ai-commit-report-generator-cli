834899d9223493b1d749dcc1b892fa05929dddcc
Sidali Assoul <assoulsidali@gmail.com>
2024-12-22 14:21:40 +0100

    fix: in json store factory

--- json-store.factory.ts
+++ json-store.factory.ts
@@ -1,26 +1,27 @@
 import { JsonStore } from "./json-local-cache";
 
-export class JsonStoreFactory{
-    private instances:Record<string,JsonStore> = {}
+export class JsonStoreFactory {
+    private instances: Record<string, JsonStore> = {}
 
-    constructor(){
+    constructor() {
 
     }
 
-    async createStore(uniqueName:string){
-    if(this.instances[uniqueName]){
-        console.error(`Store already exists for this path ${uniqueName}`)
-    }
-    const cacheStore = new JsonStore({
-        path: `./${uniqueName}cache.json`
-    }) 
-    await cacheStore.initCache()
+    async createOrGetStore(uniqueName: string) {
+        if (this.instances[uniqueName]) {
+            console.error(`Store already exists for this path ${uniqueName}`)
+            return this.instances[uniqueName]
+        }
+        const cacheStore = new JsonStore({
+            path: `./${uniqueName}cache.json`
+        })
+        await cacheStore.initCache()
 
-    this.instances[uniqueName] = cacheStore
+        this.instances[uniqueName] = cacheStore
 
-    return this.instances[uniqueName]
+        return this.instances[uniqueName]
     }
-    async getStore(repoPath:string){
+    async getStore(repoPath: string) {
         return this.instances[repoPath]
     }
 }
