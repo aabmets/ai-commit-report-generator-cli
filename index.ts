680424a4e3b960df38a8ba70c48ddf28d7b1fc46
Sidali Assoul <assoulsidali@gmail.com>
2024-12-22 11:28:10 +0100

    feat: json store factory

--- index.ts
+++ index.ts
@@ -8,21 +8,17 @@ import { Commit, CommitStatisticEntry } from './schemas';
 import { CommitSummary } from './commitSummary';
 import { JsonStore } from './json-local-cache';
 import { DailyReportAIGenerator } from './reportAIGenerator';
+import { JsonStoreFactory } from './json-store.factory';
 
 dotenv.config();
 
 const getCommitSummariesKey = (key: string) => `summaries:${key}`
 const getAIReportKey = (commits:Commit[]) => `reports:${commits.map(c=>c.hash).join(',')}`
 async function main() {
+    const repoPath = "."
+    const jsonStoreFactory = new JsonStoreFactory()
 
-    const repoPath = "../../spitha-blog";
-    const cacheStore = new JsonStore({
-        path: `./${repoPath}cache.json`
-    }) 
-    await cacheStore.initCache()
-
-
-
+    const cacheStore = await jsonStoreFactory.createStore("report-generation-app")
 
     const options = await promptForOptions(repoPath);
     console.log(`Fetching commits from ${format(options.dateRange.startDate, 'yyyy-MM-dd')} to ${format(options.dateRange.endDate, 'yyyy-MM-dd')}`);
---
+++ json-store.factory.ts
@@ -0,0 +1,26 @@
+import { JsonStore } from "./json-local-cache";
+
+export class JsonStoreFactory{
+    private instances:Record<string,JsonStore> = {}
+
+    constructor(){
+
+    }
+
+    async createStore(uniqueName:string){
+    if(this.instances[uniqueName]){
+        console.error(`Store already exists for this path ${uniqueName}`)
+    }
+    const cacheStore = new JsonStore({
+        path: `./${uniqueName}cache.json`
+    }) 
+    await cacheStore.initCache()
+
+    this.instances[uniqueName] = cacheStore
+
+    return this.instances[uniqueName]
+    }
+    async getStore(repoPath:string){
+        return this.instances[repoPath]
+    }
+}
